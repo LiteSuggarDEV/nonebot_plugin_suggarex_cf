ab731f044027b5e3b9c79ecd5af82d9ac3028c0d
JohnRichard4096 <windowserror@163.com>
2025-03-14 23:02:49 +0800

    add meta

--- src/nonebot_plugin_suggaradapter_cf/__init__.py
+++ src/nonebot_plugin_suggaradapter_cf/__init__.py
@@ -1,5 +1,14 @@
 from nonebot.plugin import PluginMetadata
-from nonebot.plugin import require
+from nonebot.plugin import require, PluginMetadata
+
+__plugin_meta__ = PluginMetadata(
+    name="适用于SuggarChat的CloudFlare Workers AI 协议适配器",
+    description="CloudFlare Adapter for suggarchat",
+    usage="",
+    type="library",
+    homepage="https://github.com/JohnRichard4096/nonebot_plugin_suggaradapter_cf",
+    supported_adapters={"~onebot.v11"},
+)
 
 require("nonebot_plugin_suggarchat")
 
