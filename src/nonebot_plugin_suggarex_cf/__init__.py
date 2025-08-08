35dc4030382a6a1b577d57392210dfab82a1559a
John Richard <windowserror@163.com>
2025-08-08 09:30:08 +0800

    适配SuggarChat最新版本

--- src/nonebot_plugin_suggarex_cf/__init__.py
+++ src/nonebot_plugin_suggarex_cf/__init__.py
@@ -7,8 +7,8 @@ from . import core
 __all__ = ["core"]
 
 __plugin_meta__ = PluginMetadata(
-    name="SuggarChat CloudFlare协议扩展附属插件",
-    description="适用于SuggarChat插件的CloudFlare协议适配器实现，SuggarChat附属插件",
+    name="SuggarChat CloudFlare扩展",
+    description="SuggarChat的CloudFlare WorkersAI API接口扩展",
     usage="",
     type="library",
     homepage="https://github.com/LiteSuggarDEV/nonebot_plugin_suggarex_cf",
--- src/nonebot_plugin_suggarex_cf/core.py
+++ src/nonebot_plugin_suggarex_cf/core.py
@@ -1,61 +1,79 @@
+from collections.abc import Iterable
+from typing import Any
+
 import aiohttp
 from aiohttp import ClientSession
 from nonebot import get_driver, logger
-from nonebot.adapters import Bot
-from nonebot_plugin_suggarchat.API import Adapter, config_manager
-from nonebot_plugin_suggarchat.config import Config
-from nonebot_plugin_suggarchat.hook_manager import register_hook
+from nonebot_plugin_suggarchat.API import ModelAdapter, config_manager
+from pydantic import BaseModel
 
 
-async def adapter(
-    base_url: str,
-    model: str,
-    key: str,
-    messages: list,
-    max_tokens: int,
-    config: Config,
-    bot: Bot,
-) -> str:
-    if config.preset == "default":
-        user_id = config.default_preset.extra.cf_user_id
-    else:
-        models = await config_manager.get_models()
-        for m in models:
-            if m.name == config.preset:
-                user_id = m.extra.cf_user_id
-                break
-        else:
-            user_id = config.default_preset.extra.cf_user_id
-            logger.warning(f"模型 {config.preset} 未找到，使用默认配置 {user_id}")
-    headers = {
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-        "Authorization": f"Bearer {key}",
-    }
-    if model.startswith("@"):
-        model = model.replace("@", "")
-    if not key:
-        raise ValueError("请配置Cloudflare API Key")
+class ResponseData(BaseModel):
+    """
+    Cloudflare API 响应数据模型
+    """
 
-    async with ClientSession(
-        headers=headers,
-        timeout=aiohttp.ClientTimeout(total=25),
-    ) as session:
-        try:
-            response = await session.post(
-                url=f"https://api.cloudflare.com/client/v4/accounts/{user_id}/ai/run/@{model}",
-                json={"messages": messages},
-            )
-            if response.status != 200:
-                logger.error(f"请求失败！user{user_id}/模型 {model}")
-                raise Exception(f"{response.status}\n{response.text}")
+    response: str
 
-            data = await response.json()
-            return data["result"]["response"]
-        except Exception as e:
-            logger.error(f"{e}")
-            logger.error("请求失败！")
-            raise e
+
+class Response(BaseModel):
+    """
+    Cloudflare API 响应模型
+    """
+
+    result: ResponseData
+    success: bool
+    messages: list[str]
+    errors: list[str]
+
+
+class CloudflareAdapter(ModelAdapter):
+    """
+    Cloudflare AI 适配器
+    """
+
+    async def call_api(self, messages: Iterable[Any]) -> str:
+        config = self.config
+        preset = self.preset
+        key = preset.api_key
+        model = preset.model
+        user_id = getattr(preset.extra, "cf_user_id") or getattr(
+            config.default_preset.extra, "cf_user_id"
+        )
+        headers = {
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+            "Authorization": f"Bearer {key}",
+        }
+        if model.startswith("@"):
+            model = model.replace("@", "")
+        if not key:
+            raise ValueError("请配置Cloudflare API Key")
+
+        async with ClientSession(
+            headers=headers,
+            timeout=aiohttp.ClientTimeout(total=25),
+        ) as session:
+            try:
+                response = await session.post(
+                    url=f"https://api.cloudflare.com/client/v4/accounts/{user_id}/ai/run/@{model}",
+                    json={"messages": messages},
+                )
+                if response.status != 200:
+                    response.raise_for_status()
+
+                data = Response.model_validate(await response.json())
+                if not data.success:
+                    raise Exception((data.errors, data.messages))
+                return data.result.response
+            except Exception as e:
+                logger.error(f"{e}")
+                logger.error("请求失败！")
+                raise e
+
+    @staticmethod
+    def get_adapter_protocol() -> str | tuple[str, ...]:
+        return "cloudflare", "cf"
 
 
 driver = get_driver()
@@ -66,13 +84,9 @@ async def hook():
     """
     启动时注册
     """
-    register_hook(init_config)
-
 
 async def init_config():
     """
     注册配置项
     """
-    ada = Adapter()
-    config_manager.reg_model_config("cf_user_id")
-    ada.register_adapter(adapter, "cf")
+    config_manager.reg_model_config("cf_user_id", "")
